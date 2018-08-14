58b7c8e13476672d900ea0e1068386ae8faddd87
daniel-browning <daniel.d.browning@gmail.com>
2018-08-14 10:15:53 -0500

    Loops Pushup;

---
+++ js/loops-pushup.js
@@ -0,0 +1,18 @@
+"use strict";
+
+ var fizzBuzz = function (num) {
+     for (var i = 1; i <= num; i++) {
+         if (i % 15 === 0) {
+             console.log("fizzbuzz");
+         } else if (i % 5 === 0) {
+             console.log("buzz");
+         } else if (i % 3 === 0) {
+             console.log("fizz");
+         } else {
+             console.log(i);
+         }
+     }
+ }
+ fizzBuzz(100);
+
+
